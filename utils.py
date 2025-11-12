2c70b3412a2255dc5e972d373ebff490644492e7
Dylan Kotliar <dylkot@gmail.com>
2025-11-12 11:15:36 -0500

    added ll gate to biaxial plot

--- utils.py
+++ utils.py
@@ -122,6 +122,10 @@ def gate_biaxial(data, g1, g2, ind=None, vertical_gate=None, horizontal_gate=Non
         elif quadrants_to_plot == 'ul':
             ax.hlines(y=quadrant_gate['vthresh'], xmin=xlim[0], xmax=quadrant_gate['hthresh'], linestyle='--', color='k', linewidth=1)
             ax.vlines(x=quadrant_gate['hthresh'], ymin=quadrant_gate['vthresh'], ymax=ylim[1]+1, linestyle='--', color='k', linewidth=1)
+        elif quadrants_to_plot == 'll':
+            ax.hlines(y=quadrant_gate['vthresh'], xmin=xlim[0], xmax=quadrant_gate['hthresh'], linestyle='--', color='k', linewidth=1)
+            ax.vlines(x=quadrant_gate['hthresh'], ymin=ylim[0]-1, ymax=quadrant_gate['vthresh'], linestyle='--', color='k', linewidth=1)
+
 
         indh = data.loc[ind, g1]> quadrant_gate['hthresh']
         indv = data.loc[ind, g2]> quadrant_gate['vthresh']
